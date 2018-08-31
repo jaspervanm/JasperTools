bdc956f010876d03e4f593a219f19c8a81ea42ab
Jasper van Miert (UMCG) <j.h.a.van.miert@umcg.nl>
2018-08-31 09:15:39 +0200

    Finish qoac_statistics
    This is now more a function to call the other functions

--- R/qoac_statistics.R
+++ R/qoac_statistics.R
@@ -3,7 +3,7 @@
 #' Calculate summary measures for quality of anticoagulation (QOAC) with VKA on a certain time period.
 #' Calculates the time within the therapeutic range (TTR) based on the Rosendaal method,
 #' the variance growth rate according to Cannegieter,
-#' and the sdtINR according to Lind (2012).
+#' and the sdtINR according to Lind (2012), using calc_ttr(), calc_vgr() and calc_sdtINR() respectively.
 #' INR measurements more than 56 days apart will not be interpolated.
 #' @param INR_meas Data.frame with (at least) the following columns:
 #'
@@ -26,13 +26,24 @@ qoac_statistics <- function( INR_meas,
 							 from = to - 365,
 							 range.lower = 2, range.upper = 3,
 							 period_id = NA_character_) {
-	
+
 	From <- to_date( pmin(from, to) )
 	To   <- to_date( pmax(from, to) )
-	
+
+	if(all(is.na(period_id))) {
+		period_id <- as.character(seq_along(period_id))
+	}
+	periods <- tibble(
+		period_id = period_id,
+		from      = From,
+		to        = To
+	)
+
 	INR_meas$INR_date <- to_date(INR_meas$INR_date)
 	INR_meas <- as.data.table(INR_meas, key = "INR_date")
 	# this automatically sorts on INR_date
+	INR_meas[ INR > 10, INR := 10 ]
+	INR_meas[ INR < 0.8, INR := 0.8 ]
 
 	starters <- map_dbl( From, ~detect_index( INR_meas$INR_date, `>=`, . ) )
 	stoppers <- map2_dbl(  To, starters,
@@ -46,123 +57,30 @@ qoac_statistics <- function( INR_meas,
 		warning("Geen INR's beschikbaar voor periode", map2(to, from, ~paste(.x, "-", .y)), call. = FALSE)
 		starters <- starters[ !no_meas ]
 		stoppers <- stoppers[ !no_meas ]
+		From <- From[ !no_meas ]
+		To   <- To[ !no_meas ]
 	}
 
 	rows <- map2( starters, stoppers, ~sort( c( .x, .y) ) ) %>%
 		map( . , ~seq( .[1], .[2] ) )
+	names(rows) <- period_id[ !no_meas ]
 
-	### iTTR
-	ittr <- pmap_df(
+	ittr <- pmap_dfr(
 		list(
-			R = rows,
-			from = From[ !no_meas ],
-			to = To[ !no_meas ]
-		),
-		function( R, from, to ) {
-			from <- as.Date( from, origin = "1970-01-01")
-			to <- as.Date( to, origin = "1970-01-01")
+			R = map(rows, ~append( . , min( . ) - 1, 0)), from = From, to = To ),
+		~calc_ttr(INR_meas[ R ], range.lower, range.upper, to, from ),
+		# we use append so calc_ttr will be fed some extra data to interpolate from history to to
+		.id = "period_id"
+	) %>%
+		left_join(periods, by = "period_id")
 
-			Data <- R %>%
-				c(
-					ifelse( min(.) <= 1, NA, min(.) - 1 ),
-					. ,
-					ifelse(interpolate_end, max(.) + 1, NA )
-				) %>%
-				slice( INR_meas, . ) %>%
-				within(
-					INR_date <- trunc( INR_date ), # let's use only days
-					INR[ INR > 10] <- 10
-				) %>%
-				group_by( INR_date ) %>%
-				dplyr::summarise( INR = mean( INR ) ) %>%
-				ungroup()
+	variability <- map_dfr(rows, ~tibble(
+		vgr = calc_vgr(INR_meas[ .x ]),
+		sdtINR = calc_sdtINR(INR_meas [ .x ])
+	), .id = "period_id") %>%
+		left_join(periods, by = "period_id")
 
-			if(nrow(Data) < 2) {
-				warning("Onvoldoende INR's voor een TTR")
-				return( data.frame( from, to ) )
-			}
+	output <- full_join( ittr, variability, by = c("from", "to", "period_id") )
 
-			diff_date <- Data$INR_date %>%
-				diff( units = "days" ) %>%
-				as.integer()
-			use_date <- sapply( diff_date, function(x) {
-				c( TRUE, rep( x <= MAX_BETWEEN, x - 1 ) )
-			} ) %>%
-				unlist() %>%
-				c( . , TRUE)
-
-			trim_dates <- c(
-				as.integer( from - min( Data$INR_date ) ) %>%
-					max( . , 0 ) %>%
-					seq_len(),
-				as.integer( max( Data$INR_date ) - to ) %>%
-					max( . , 0 ) %>%
-					seq_len( . ) * -1 + length( use_date ) + 1
-			)
-			use_date[ trim_dates ] <- FALSE
-
-			interpolated_INR <- with( Data, {
-				approx( INR_date, INR,
-						(min( Data$INR_date ) -1 + seq_along( use_date ) )[ use_date ] )
-			})$y
-
-			tibble(
-				below_range = mean(interpolated_INR < range.lower),
-				in_range    = mean(interpolated_INR <= range.upper) - below_range,
-				above_range = mean(interpolated_INR > range.upper),
-				mean_inr    = mean(interpolated_INR),
-				from        = from,
-				to          = to,
-				min_date    = min( Data$INR_date ),
-				max_date    = max( Data$INR_date )
-			)
-		} )
-
-	variability <- pmap_df(
-		list(
-			R = rows,
-			from = From[ !no_meas ],
-			to = To[ !no_meas ]
-		),
-		function( R, from, to ) {
-			from <- as.Date( from, origin = "1970-01-01")
-			to <- as.Date( to, origin = "1970-01-01")
-
-			INR_meas[ R , ] %>%
-				mutate(
-					t_inr = transform_norm( INR ) %>%
-						pmax( . , 0.8) %>%
-						pmin( . , 10 ),
-					tt    = as.integer( difftime( INR_date, min( INR_date ), units = "days" ) )
-				) %>%
-				dplyr::filter( t_inr > 0, t_inr < 10 ) %>%
-				do({
-					reg_line <- try( lm( t_inr ~ tt, . ), silent = TRUE)
-
-					tibble(
-						sdtINR = ifelse( class(reg_line) == "lm",
-										 sqrt( deviance(reg_line) / df.residual(reg_line)),
-										 NA ),
-						VGR = ifelse( nrow( . ) > 1,
-									  sum( diff( .$INR ) ^ 2 / as.numeric( diff( .$INR_date ) / 7 ) ) / length( .$INR ) ,
-									  NA ),
-						from = from,
-						to   = to
-					)
-				})
-		} )
-
-	output <- full_join( ittr, variability, by = c("from", "to") )
-
-	if(all(is.na(period_id))) {
-		return( output )
-	} else {
-		left_join(
-			output,
-			data.frame( period_id,
-						from = as.Date(from, origin = "1970-01-01"),
-						to = as.Date(to, origin = "1970-01-01")),
-			by = c("from", "to")
-		)
-	}
+	output
 }
