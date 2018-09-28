2678a39fca45295de46e98e4961fce571c97d77f
Jasper van Miert <repsaj@gmail.com>
2018-09-28 19:53:38 +0200

    Expand mean_sd() with median_iqr()

--- R/mean_sd.R
+++ R/mean_sd.R
@@ -1,17 +1,22 @@
 #' Summarise a numerical vector to mean (SD).
 #'
-#' Summarise a numerical vector to a mean and a SD.
+#' Summarise a numerical vector to a mean and a SD,
+#' or a median and an interquartile range (p25 and p75).
 #'
 #' @import dplyr
 #' @param x Numerical vector
 #' @param digits Number of decimals to use
 #' @return Character vector
-#' @importFrom stats sd
+#' @importFrom stats sd quantile median
 #' @export
 #' @examples
 #' mean_sd(1:10)
+#' median_iqr(1:10)
 
 mean_sd <- function(...) UseMethod("mean_sd")
+
+#' @export
+median_iqr <- function(...) UseMethod("median_iqr")
 
 #' @export
 #' @importFrom stats sd
@@ -24,4 +29,17 @@ mean_sd.numeric <- function(x, ...) {
 mean_sd.matrix <- function(x, digits = 1, ...) {
 	x[] <- format_number(x, digits)
 	paste0(x[, 1], " (", x[, 2], ")")
+}
+
+#' @export
+#' @importFrom stats median quantile
+median_iqr.numeric <- function(x, ...) {
+	cbind(M = median(x), p25 = quantile(x, 0.25), p75 = quantile(x, 0.75)) %>%
+		median_iqr(...)
+}
+
+#' @export
+median_iqr.matrix <- function(x, digits = 1, ...) {
+	x[] <- format_number(x, digits)
+	paste0(x[, 1], " (IQR ", x[, 2], " -- ", x[, 3], ")")
 }
