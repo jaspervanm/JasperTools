cb230acbfc566695e9d3f4e32f6f404bc1da6903
Jasper van Miert <repsaj@gmail.com>
2019-08-03 09:55:54 +0200

    Tweak digits for p value

--- R/format_pval.R
+++ R/format_pval.R
@@ -3,16 +3,19 @@
 #' Formats a p value to a number of digits, or <X
 #'
 #' @param x Numerical vector
-#' @param digits Decimals to show
+#' @param digits Digits to show
 #' @param small Threshold to show "smaller than X"
 #' @return Character vector
 #' @export
 #' @examples
 #' format_pval(0.0001)
 
-format_pval <- function(x, digits = 3, small = 10^(-1 * digits)) {
+format_pval <- function(x, digits = 2, small = 10^(-1 * (digits + 1))) {
 	ifelse(x < small,
 		   paste0("<", small),
-		   round(x, digits)
+		   ifelse( x < 10^(-1 * digits),
+		   		round(x, digits + 1),
+		   		round(x, digits)
+		   )
 	)
 }
